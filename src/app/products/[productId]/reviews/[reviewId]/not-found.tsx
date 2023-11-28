e296e497ef14452e0061502c89d65a3862116449
AnPham <anpham1122000@gmail.com>
2023-11-28 22:46:44 +0700

    not found page

---
+++ src/app/products/[productId]/reviews/[reviewId]/not-found.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function NotFound() {
+  return (
+    <h2>Review not found</h2>
+  )
+}
+
+export default NotFound
--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,8 +1,13 @@
+import { notFound } from 'next/navigation';
 import React from 'react'
 
 function Review({params}: {
   params: {reviewId: string; productId: string}
 }) {
+  if(parseInt(params.reviewId) > 1000) {
+    notFound()
+  }
+
   console.log(params.reviewId);
   console.log(params.productId)
   return (
